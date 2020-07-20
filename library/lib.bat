5dc0b8183f73aac93e46e39d61d6eb4c8e102289
Rakesh Singh <rakeshsingh0018@gamil.com>
2020-07-20 18:11:27 +0530

    second commit

--- library/lib.bat
+++ library/lib.bat
@@ -1,6 +1,3 @@
 pip install pyYAML
 pip install robotframework
-pip install selenium
 pip install robotframework-seleniumlibrary
-pip install robotframework-selenium2library
-robot -d results rs_testcases.robot
