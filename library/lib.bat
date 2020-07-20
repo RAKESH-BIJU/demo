8e4c74ac843f3f195851f4034c2e032eb8421216
Rakesh Singh <rakeshsingh0018@gamil.com>
2020-07-20 12:24:51 +0530

    First commit

--- library/lib.bat
+++ library/lib.bat
@@ -1,4 +1,5 @@
 pip install pyYAML
 pip install robotframework
-pip install robotframework-seleniumlibrary
+pip install robotframework-selenium
+pip install robotframework-selenium2library
 robot -d results/ -e buying_shirt rs_testcases.robot
